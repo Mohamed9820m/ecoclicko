ff4cfe094fd5e82a41676078ce474b0367d22dd4
Mohamed9820m <mohamedhabibmarouani8@gmail.com>
2023-10-27 00:12:35 +0100

    final version

--- backend/nodemailer.js
+++ backend/nodemailer.js
@@ -27,7 +27,7 @@ module.exports.sendConfirmationEmail = (
     
         <p style="color: #555;">Click Here To Confirm Your Email :</p>
     
-        <a href="http://localhost:3000/confirm/${confirmationCode}" style="background-color: #009e00; color: white; padding: 15px 30px; text-decoration: none; border-radius: 5px; display: inline-block; margin-top: 20px;">Click Here To Activate.</a>
+        <a href="https://ecoclicko.onrender.com/confirm/${confirmationCode}" style="background-color: #009e00; color: white; padding: 15px 30px; text-decoration: none; border-radius: 5px; display: inline-block; margin-top: 20px;">Click Here To Activate.</a>
       </div>
     </div>
     
@@ -56,7 +56,7 @@ module.exports.sendPasswordResetEmail = (email, resetCode) => {
       
           <p style="color: #555;">To reset your password, please click on the link below:</p>
       
-          <a href="http://localhost:3000/reset-password/${resetCode}" style="background-color: #4CAF50; color: white; padding: 15px 30px; text-decoration: none; border-radius: 5px; display: inline-block; margin-top: 20px;">Click here to reset your password</a>
+          <a href="https://ecoclicko.onrender.com/reset-password/${resetCode}" style="background-color: #4CAF50; color: white; padding: 15px 30px; text-decoration: none; border-radius: 5px; display: inline-block; margin-top: 20px;">Click here to reset your password</a>
         </div>
       </div>
         `,
